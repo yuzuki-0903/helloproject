029ecc716ea25599d7be7922488736deba5a7289
yuzuki-0903 <yuzukikouno0903@gmail.com>
2024-03-31 19:35:13 +0900

    helloworldapp用に修正

--- urls.py
+++ urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import helloworldfunc, HelloWorldClass
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworldurl/', helloworldfunc),
-    path('helloworldurl2/', HelloWorldClass.as_view()),
+    path('', include('helloworldapp.urls')),
 ]
