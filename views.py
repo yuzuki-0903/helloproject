0aacd78feed75c2ef4e0f5b80b95812b46fe63b8
yuzuki-0903 <yuzukikouno0903@gmail.com>
2024-03-31 18:23:05 +0900

    helloworld2追加

--- views.py
+++ views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 def helloworldfunc(request):
-    responseobject = HttpResponse('hello world')
+    responseobject = HttpResponse('<h1>hello world</h1>')
     return responseobject
+
+
+class HelloWorldClass(TemplateView):
+    template_name = 'hello.html'
